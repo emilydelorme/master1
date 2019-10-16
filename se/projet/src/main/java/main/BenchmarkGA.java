b9e4dd071b1c4246fe9b8cce700f581ce492075e
Emily Delorme <emilydelormefr@gmail.com>
2019-10-16 11:42:09 +0200

    Add SE project

---
+++ se/projet/src/main/java/main/BenchmarkGA.java
@@ -0,0 +1,10 @@
+package main;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class BenchmarkGA {
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
